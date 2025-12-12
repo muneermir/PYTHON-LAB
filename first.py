433c75fc3ef96ee9afa635e07495ca4b68e8d0ef
MUNEER HUSSAIN MIR <90671155+muneermir@users.noreply.github.com>
2025-12-12 18:05:51 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,9 @@
+print("Hello world")
+import pyjokes  #module
+# print("printing jokes for u ....")  i use control+ forward slash (/)
+joke =pyjokes.get_joke()
+print(joke)
+
+
+# how can i solve the problem
+# i will do best to do that
